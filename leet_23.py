a0c6ff2e63ab89c5a2f1905700f376d790a8d5c2
khan2626 <israelkhan217@gmail.com>
2025-01-16 11:44:26 +0100

    best time to trade stock added

---
+++ leet_23.py
@@ -0,0 +1,14 @@
+
+def best_time_to_trade_stock(prices):
+    buy_price = prices[0]
+    profit = 0
+
+    for price in prices:
+        if buy_price > price:
+            buy_price = price
+        profit = max(profit, price - buy_price)
+    return profit
+
+if __name__ == '__main__':
+    prices = [7,1,3,5,4]
+    print(best_time_to_trade_stock(prices))
