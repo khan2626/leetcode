ce98b65f02d91b6ef7a249e1737bbb0101061abf
khan2626 <israelkhan217@gmail.com>
2025-01-23 16:45:49 +0100

    reverse int solved

---
+++ leet_29.py
@@ -0,0 +1,21 @@
+
+def reverse_int(x):
+
+    MAX_INT = 2 ** 31 -1
+    MIN_INT = -2 ** 31
+
+    reverse = 0
+    sign = -1 if x < 0 else 1
+    x = abs(x)
+
+    while x != 0:
+        if reverse > MAX_INT/10 or reverse < MIN_INT/10:
+            return 0
+        digit = x % 10
+        reverse = reverse * 10 + digit
+        x = x // 10
+    return sign * reverse
+
+if __name__ == '__main__':
+    x = -2147483412
+    print(reverse_int(x))
