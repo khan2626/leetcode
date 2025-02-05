6ca0dc6b3f90be7ba56a5ed200531a732e8f3e15
khan2626 <israelkhan217@gmail.com>
2025-02-05 15:59:06 +0100

    symmetric tree solved

---
+++ leet_36.py
@@ -0,0 +1,32 @@
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def isSymmetric(self, root):
+        def isSame(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            return(left.val == right.val and \
+                   isSame(left.left, right.right) and \
+                    isSame(left.right, right.left))
+        return isSame(root.left, root.right)
+    
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(2)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(4)
+root.right.left = TreeNode(4)
+root.right.right = TreeNode(3)
+
+solution = Solution()
+result = solution.isSymmetric(root)
+print(result)
